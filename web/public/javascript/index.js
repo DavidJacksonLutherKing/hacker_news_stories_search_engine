fd463341198c2f2de83180302292dc328983c6a5
David Ding <dwding@merkleinc.com>
2019-07-25 13:32:15 +0800

    Search Result Style Modification

--- web/public/javascript/index.js
+++ web/public/javascript/index.js
@@ -14,7 +14,8 @@ $("#search").click(function () {
         $('#myModal').modal('show');
     } else {
         $("#search").attr("disabled", "disabled");
-        $("#search").text("Searching...");
+        $("#search-form").attr("class",$("#search-form").attr("class")+" after-search");
+        $("#search-result").html("");
         var data = 'title=' + title + '&text=' + content + '&startdate=' + startdate + '&enddate=' + enddate;
         $.ajax({
             url: '/service/search',
@@ -29,7 +30,6 @@ $("#search").click(function () {
                 console.log(XMLHttpRequest.status);
                 console.log('status:' + status);
                 $("#search").removeAttr("disabled");
-                $("#search").text("Search");
             }
         });
     }
@@ -51,61 +51,61 @@ var SearchApp = {};
 SearchApp.searchResult = {};
 SearchApp.searchResult.resultList = [];
 SearchApp.showSearchResultList = function (data = "") {
-    //var acticles = SearchApp.searchResult.resultList =data.articles;
-    var articlesJSONString = {
-        "articles": [{
-            "title": "David Lee Roth scream box \u2013 Arduino & Adafruit WaveShield davidleeroth",
-            "URL": "http://www.adafruit.com/blog/2012/03/20/david-lee-roth-scream-box-arduino-adafruit-waveshield-davidleeroth/",
-            "text": "",
-            "date": "2012-03-21"
-        }, {
-            "title": "General david petraeus's rules for living",
-            "URL": "http://www.thedailybeast.com/newsweek/2012/11/04/general-david-petraeus-s-rules-for-living.html",
-            "text": "",
-            "date": "2012-11-10"
-        }, {
-            "title": "Gmail - Compose Mail - david.lloydjonesgmail.com",
-            "URL": "https://mail.google.com/mail/?view=cm&fs=1&to&su=myubuntuone%27s+Channel+-+YouTube&body=http://www.youtube.com/myubuntuone&ui=2&tf=1&shva=1",
-            "text": null,
-            "date": "2011-11-17"
-        }, {
-            "title": "Bo\u00eete de r\u00e9ception - herautdavidgmail.com - Gmail",
-            "URL": "https://mail.google.com/mail/u/0/?tab=wm#inbox",
-            "text": null,
-            "date": "2012-10-27"
-        }, {
-            "title": "Foodchem-david's blog",
-            "URL": "http://foodchem-david.blogspot.jp/",
-            "text": "Are Food Additives Safe? Aspartame Side Effects",
-            "date": "2012-05-08"
-        }, {
-            "title": "Cool water davidoff",
-            "URL": "http://www.beautystorage.com/catalogsearch/advanced/result/?manufacturer[]=18",
-            "text": "",
-            "date": "2012-08-06"
-        }, {
-            "title": "Cheap david krejci jersey",
-            "URL": "http://www.365djerseys.com/cheap-boston-bruins-david-krejci-jersey-46-black-2011-stanley-c-p-4711.html",
-            "text": "",
-            "date": "2012-08-06"
-        }, {
-            "title": "King david garage door repair long island",
-            "URL": "http://www.kingdavidgaragedoor.com/",
-            "text": "garage doors long island experts.professional services,and installation\ncommercial and residential",
-            "date": "2011-05-08"
-        }, {
-            "title": "Archaeologists asking for help to interpret a discovery in the city of david",
-            "URL": "http://www.cityofdavid.org.il/ArticleDetails_eng.asp?id=327",
-            "text": "",
-            "date": "2011-12-11"
-        }, {
-            "title": "Anonymous defaces davidguetta.co",
-            "URL": "http://www.davidguetta.co",
-            "text": "",
-            "date": "2012-01-26"
-        }]
-    };
-    var acticles = articlesJSONString.articles;
+    var acticles = SearchApp.searchResult.resultList =data.articles;
+    // var articlesJSONString = {
+    //     "articles": [{
+    //         "title": "David Lee Roth scream box \u2013 Arduino & Adafruit WaveShield davidleeroth",
+    //         "URL": "http://www.adafruit.com/blog/2012/03/20/david-lee-roth-scream-box-arduino-adafruit-waveshield-davidleeroth/",
+    //         "text": "",
+    //         "date": "2012-03-21"
+    //     }, {
+    //         "title": "General david petraeus's rules for living",
+    //         "URL": "http://www.thedailybeast.com/newsweek/2012/11/04/general-david-petraeus-s-rules-for-living.html",
+    //         "text": "",
+    //         "date": "2012-11-10"
+    //     }, {
+    //         "title": "Gmail - Compose Mail - david.lloydjonesgmail.com",
+    //         "URL": "https://mail.google.com/mail/?view=cm&fs=1&to&su=myubuntuone%27s+Channel+-+YouTube&body=http://www.youtube.com/myubuntuone&ui=2&tf=1&shva=1",
+    //         "text": null,
+    //         "date": "2011-11-17"
+    //     }, {
+    //         "title": "Bo\u00eete de r\u00e9ception - herautdavidgmail.com - Gmail",
+    //         "URL": "https://mail.google.com/mail/u/0/?tab=wm#inbox",
+    //         "text": null,
+    //         "date": "2012-10-27"
+    //     }, {
+    //         "title": "Foodchem-david's blog",
+    //         "URL": "http://foodchem-david.blogspot.jp/",
+    //         "text": "Are Food Additives Safe? Aspartame Side Effects",
+    //         "date": "2012-05-08"
+    //     }, {
+    //         "title": "Cool water davidoff",
+    //         "URL": "http://www.beautystorage.com/catalogsearch/advanced/result/?manufacturer[]=18",
+    //         "text": "",
+    //         "date": "2012-08-06"
+    //     }, {
+    //         "title": "Cheap david krejci jersey",
+    //         "URL": "http://www.365djerseys.com/cheap-boston-bruins-david-krejci-jersey-46-black-2011-stanley-c-p-4711.html",
+    //         "text": "",
+    //         "date": "2012-08-06"
+    //     }, {
+    //         "title": "King david garage door repair long island",
+    //         "URL": "http://www.kingdavidgaragedoor.com/",
+    //         "text": "garage doors long island experts.professional services,and installation\ncommercial and residential",
+    //         "date": "2011-05-08"
+    //     }, {
+    //         "title": "Archaeologists asking for help to interpret a discovery in the city of david",
+    //         "URL": "http://www.cityofdavid.org.il/ArticleDetails_eng.asp?id=327",
+    //         "text": "",
+    //         "date": "2011-12-11"
+    //     }, {
+    //         "title": "Anonymous defaces davidguetta.co",
+    //         "URL": "http://www.davidguetta.co",
+    //         "text": "",
+    //         "date": "2012-01-26"
+    //     }]
+    // };
+    // var acticles = articlesJSONString.articles;
     for (key in acticles) {
         var title = acticles[key].title;
         var text = acticles[key].text != null&&acticles[key].text != ''  ? acticles[key].text : '...';
@@ -121,5 +121,4 @@ SearchApp.showSearchResultList = function (data = "") {
         //itemDiv.text(title+ + "  " +date+ "\n"+ url +"\n"+text);
         $("#search-result").append(itemDiv);
     }
-}
-SearchApp.showSearchResultList();
+};
