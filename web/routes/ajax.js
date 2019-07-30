97b780d994eeed11053b8ca0584826805ad3fcb6
David Ding <dwding@merkleinc.com>
2019-07-30 09:22:57 +0800

    add instruction to ajax call

--- web/routes/ajax.js
+++ web/routes/ajax.js
@@ -5,6 +5,16 @@ var run_cmd = require('../util/run_cmd.js');
 var cache_data = require("../util/cache_data.js");
 var process_data = require("../util/process_data.js");
 var urllib = require('url');
+/**
+ * Request Handler on "/search?title=&text=&startdate=&enddate=&page=1&itemNum="
+ * parameter
+ * * text : string [title box value]
+ * * title : string [text box value]
+ * * startdata : number [data range box value parsed]
+ * * enddate : number [data range box value parsed]
+ * * page : number [default = 1]
+ * * itemNum = number [default = 50, inter code modifiable ]
+ */
 router.get('/search', function (req, res, next) {
     var para = urllib.parse(req.url, true);
     var title = para.query.title;
@@ -20,24 +30,31 @@ router.get('/search', function (req, res, next) {
     requestParameter.enddate = enddate;
     var queryDataResult = undefined;
 
+    //Check if the data has been cached first.
     fileExist = cache_data.dataFileIsExist(requestParameter);
     if (fileExist.status == true) {
+        //If data was cached, read the data file and rend data to front-end
         console.log("fetch data from cache");
         fetchData = cache_data.fetch_data(requestParameter);
         var resSendData = process_data.fetchDataByPage(JSON.parse(fetchData.data), page, itemNumInPage);
         res.send(resSendData);
     } else {
+        //If data was not cached, call run_cmd.exec to hang up child process and execute python script to get bigquery data.
         var errorMsg = "";
         var queryDataResult = 
         console.log("fetch data from bigquery")
         requestParameterString = JSON.stringify(requestParameter);
         var python_env_path = '';
         if (process.platform == 'win32') {
+            // python virtualenv path for windows 10
             python_env_path = path.join(__dirname, '../../service/hackernewsstories/Scripts/python');
         } else {
+             // python virtualenv path for Ubuntu
             python_env_path = path.join(__dirname, '../../service/hackernewsstoriesUbuntu/bin/python3.6');
         }
         console.log(requestParameterString);
+        
+        //hang up python child process
         run_cmd.exec(python_env_path, [path.join(__dirname, '../../service/search-engine/fetch-bigquery-result.py'), requestParameterString],
             function (data) {
                 queryDataResult = data;
@@ -51,6 +68,7 @@ router.get('/search', function (req, res, next) {
                 errorMsg = err;
             }
         );
+        // cached data first and then rend data to front-end
         if(queryDataResult != ""){
             var cacheData = cache_data.cache_data(requestParameter, queryDataResult, function (data) {
                 console.log('after cached data:' + data);
